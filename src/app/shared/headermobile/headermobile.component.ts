c1c9277d2d2e7f5573184dd864be1ba16a57847e
Arnold <arnoldingunza@gmail.com>
2020-10-06 16:52:10 -0500

    primer Commit

---
+++ src/app/shared/headermobile/headermobile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-headermobile',
+  templateUrl: './headermobile.component.html',
+  styles: []
+})
+export class HeadermobileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
